01bb7872bba15928f69cb4bf4910015fa56a9d32
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-20 16:29:43 -0700

    add code for the docker container based on mysql
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

---
+++ run.cmd
@@ -0,0 +1,12 @@
+docker stop fabric.realtime.mysql
+docker rm fabric.realtime.mysql
+docker build -t healthcatalyst/fabric.realtime.mysql . 
+
+docker volume rm mysqldata
+
+docker volume create --name mysqldata
+
+docker run -v mysqldata:/var/lib/mysql -d -p 3306:3306 --rm --name fabric.realtime.mysql -e MYSQL_ROOT_PASSWORD=new-password -e MYSQL_DATABASE=nlpmt -e MYSQL_USER=NLP_APP_USER -e MYSQL_PASSWORD=yourpassword -t healthcatalyst/fabric.realtime.mysql
+
+sleep 5
+
